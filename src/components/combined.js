1bb7c50f27aa1a871cb8c6cce59d8997af176b65
Aditya <addy.7kad@gmail.com>
2020-04-11 19:53:02 +0530

    added total cases

--- src/components/combined.js
+++ src/components/combined.js
@@ -5,13 +5,26 @@ import StateTable from "./stateTable";
 
 const Combined = () => {
   const [stats, setStats] = useState([]);
+  const [totalCases, setTotalCases] = useState(0);
 
   useEffect(() => {
-    console.log("render");
     axios.get("https://api.covid19india.org/data.json").then((response) => {
       setStats(response.data.statewise);
       //   console.log(stats[0]);
     });
+    axios
+      .get("https://api.covid19india.org/state_test_data.json")
+      .then((response) => {
+        let filteredCases = response.data.states_tested_data.map((item) => {
+          return parseInt(item.totaltested !== "" ? item.totaltested : 0);
+        });
+
+        let totalNoOfCases = filteredCases.reduce((accu, sum) => {
+          return accu + sum;
+        }, 0);
+
+        setTotalCases(totalNoOfCases);
+      });
   }, []);
 
   return (
@@ -33,6 +46,10 @@ const Combined = () => {
         <div>
           <h2>Recovered</h2>
           <h3>{stats[0] ? stats[0].recovered : "-"}</h3>
+        </div>
+        <div>
+          <h2>Total Tests</h2>
+          <h3>{totalCases}</h3>
         </div>
       </div>
       <StateTable stateData={stats} />
