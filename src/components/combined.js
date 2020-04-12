1d55ceae8d295aaed45f2a40cbb151566284806b
Aditya <addy.7kad@gmail.com>
2020-04-12 13:28:22 +0530

    added statewise total cases

--- src/components/combined.js
+++ src/components/combined.js
@@ -8,22 +8,52 @@ const Combined = () => {
   const [totalCases, setTotalCases] = useState(0);
 
   useEffect(() => {
-    axios.get("https://api.covid19india.org/data.json").then((response) => {
-      setStats(response.data.statewise);
-      //   console.log(stats[0]);
-    });
+    let finalStateData;
+    // Total Test Cases
     axios
       .get("https://api.covid19india.org/state_test_data.json")
       .then((response) => {
-        let filteredCases = response.data.states_tested_data.map((item) => {
-          return parseInt(item.totaltested !== "" ? item.totaltested : 0);
+        let filterStateTest = response.data.states_tested_data.map((item) => {
+          let formatdate = item.updatedon;
+          let DateArray = formatdate.split("/");
+          [DateArray[0], DateArray[1]] = [DateArray[1], DateArray[0]];
+          let finalDate = DateArray.join("/");
+          return { ...item, updatedon: finalDate };
         });
 
+        //Getting latest total cases tests
+        let stateWiseTest = filterStateTest
+          .reverse()
+          .filter(
+            (item, index, self) =>
+              index ===
+              self.findIndex(
+                (t) => t.state === item.state && t.totaltested !== ""
+              )
+          );
+
+        //Setting total tested cases to 0 for the state where there are no case tested
+        let filteredCases = stateWiseTest.map((item) => {
+          return parseInt(item.totaltested !== "" ? item.totaltested : 0);
+        });
+        //Overall Country Tests
         let totalNoOfCases = filteredCases.reduce((accu, sum) => {
           return accu + sum;
         }, 0);
-
         setTotalCases(totalNoOfCases);
+
+        //Getting State wise data plus adding latest totalcase tested to every state
+        axios.get("https://api.covid19india.org/data.json").then((response) => {
+          finalStateData = response.data.statewise;
+          for (let i = 0; i < finalStateData.length; i++) {
+            for (let j = 0; j < stateWiseTest.length; j++) {
+              if (finalStateData[i].state === stateWiseTest[j].state) {
+                finalStateData[i].totaltested = stateWiseTest[j].totaltested;
+              }
+            }
+          }
+          setStats(finalStateData);
+        });
       });
   }, []);
 
--- src/components/stateTable.js
+++ src/components/stateTable.js
@@ -1,11 +1,10 @@
 import React, { useMemo } from "react";
 
-const StateTable = props => {
+const StateTable = (props) => {
   const storeData = useMemo(() => props.stateData, [props.stateData]);
   const filteredData = storeData.filter(
     (item, index) => item.state !== "Total"
   );
-
   return (
     <div>
       <table>
@@ -16,17 +15,19 @@ const StateTable = props => {
             <td>Confirmed</td>
             <td>Deaths</td>
             <td>Recovered</td>
+            <td>Total Tests</td>
           </tr>
         </thead>
         <tbody>
           {filteredData
-            ? filteredData.map(dt => (
+            ? filteredData.map((dt) => (
                 <tr key={dt.state}>
                   <td>{dt.state}</td>
                   <td>{dt.active}</td>
                   <td>{dt.confirmed}</td>
                   <td>{dt.deaths}</td>
                   <td>{dt.recovered}</td>
+                  <td>{dt.totaltested ? dt.totaltested : "-"}</td>
                 </tr>
               ))
             : null}
