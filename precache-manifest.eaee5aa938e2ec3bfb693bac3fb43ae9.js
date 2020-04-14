7af192ccf53df582646b9cfb83f5959e1a4d7f58
Aditya <addy.7kad@gmail.com>
2020-04-14 20:18:32 +0530

    Updates

---
+++ precache-manifest.eaee5aa938e2ec3bfb693bac3fb43ae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e58bed5905a71b0b6fb6d2c3239ceb",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "898d5ecc23aa010d4972",
+    "url": "/covid-stats/static/css/main.55a66a5e.chunk.css"
+  },
+  {
+    "revision": "aaaa361934da8c06bc18",
+    "url": "/covid-stats/static/js/2.a7cd8984.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-stats/static/js/2.a7cd8984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898d5ecc23aa010d4972",
+    "url": "/covid-stats/static/js/main.f858d3f2.chunk.js"
+  },
+  {
+    "revision": "424e1018234cf4e3cffd",
+    "url": "/covid-stats/static/js/runtime-main.f1aa86e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/authorization.js
+++
@@ -1,4 +0,0 @@
-export const authorization = {
-  "X-RapidAPI-Host": "coronavirus-monitor.p.rapidapi.com",
-  "X-RapidAPI-Key": "68781a08bcmsh8884703f7285062p1d71e1jsnf3468378e101",
-};
